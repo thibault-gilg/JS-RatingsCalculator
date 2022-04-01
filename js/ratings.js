1c0e68528063f947db860c995807aec0becba88c
Thibault GILG <thibault.gilg@cpe.fr>
2022-04-01 16:50:37 +0200

    final push

---
+++ js/ratings.js
@@ -0,0 +1,30 @@
+function collect_ratings() {
+
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+    let rating = 0;
+
+    const elements = document.querySelectorAll(".rating");
+    elements.forEach(function (element) {
+        rating = parseInt(element.id.replace('star', ''));
+        // console.log(rating, typeof rating);
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    }
+    );
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
+}
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    let average = document.querySelector('#average');
+    average.value = ratings.average.toFixed(2);
+});
